d612b698b29d886f2cc8af00ebd2f77400865676
Adam Coffman <acc@fastmail.com>
2024-07-12 15:44:37 -0500

    additional deployment updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,10 +7,15 @@ set :rails_env, 'production'
 
 append :linked_files, 'config/master.key'
 
-# if !ENV['CI']
-#   set :ssh_options, {
-#     keys: ENV['CIVIC_PROD_KEY'],
-#     forward_agent: false,
-#     auth_methods: %w(publickey)
-#   }
-# end
+# from https://github.com/capistrano/rails?tab=readme-ov-file#uploading-your-masterkey
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app) do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
