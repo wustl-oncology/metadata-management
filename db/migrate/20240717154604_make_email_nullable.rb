8f949e7ef08adb8eb665b85a141ca0c1c66b8313
Adam Coffman <acc@fastmail.com>
2024-07-17 11:23:21 -0500

    make email a nullable field. github doees not always include it

---
+++ db/migrate/20240717154604_make_email_nullable.rb
@@ -0,0 +1,5 @@
+class MakeEmailNullable < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :users, :email, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_160627) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_17_154604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -310,7 +310,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_160627) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "email", null: false
+    t.text "email"
     t.text "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
